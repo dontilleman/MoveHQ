babd24fa34c24a2ff6ab9e5934a5b63f37b9dd45
Don Tilleman <dontilleman@users.noreply.github.com>
2022-03-04 12:20:09 -0700

    new(frontend): check for user entry and add error styling

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+    require_once('.scripts/users.inc.php');
     session_start();
+    $_SESSION['loggedIn'] = false;
+    $_SESSION['error'] = '';
+    if(!empty($_POST)) {
+      if($_POST['userName'] && $_POST['password']){
+        $_SESSION['loggedIn'] = true;
+        header('Location: task-list.php');
+      } else {
+        $_SESSION['loggedIn'] = false;
+        $_SESSION['error'] = 'Please enter your User Name and Password';
+      }
+    }
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,6 +27,16 @@
   <body>
     <div id ='wrap'>
       <h1>Login and Manage your task list</h1>
+      <?php 
+      if(strlen($_SESSION['error']) > 0) {
+        echo "<div id='error'>".$_SESSION['error']."</div>\n";
+      }
+      ?>
+      <form action='' id='login' method='post'>
+        <label for='userName'>User Name:<input type='text' id='userName' name='userName' /> <br />
+        <label for='password'>Password:<input type='password' id='password' name='password' />
+        <input type="submit" value="Login">
+      </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
